ee23d73712ff4164901ca520b1c68f2e7226e5f6
Francisco Neto <127354630+Frank7852@users.noreply.github.com>
2025-03-28 11:41:06 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-//document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     const userLang = navigator.language || navigator.userLanguage;
 
     if (userLang.startsWith("pt")) {
-        window.location.href = "../pages/pt.html"; // Redireciona para a página em português
+        window.location.href = "./pages/pt.html";
     } else {
-        window.location.href = "../index.html"; // Redireciona para a página em inglês
+        window.location.href = "./index.html";
     }
 });
